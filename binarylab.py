d6fe59fcc303245d01153d361ecc1a1e20fb2188
judynjagi <judynjagi@gmail.com>
2016-12-04 03:34:36 +0300

    Searching
    Binary Search

---
+++ binarylab.py
@@ -0,0 +1,30 @@
+
+class ListComprehension(object):
+	list=[]
+	length=None
+	def _init_(self, a,b):
+		self.a=len(list)
+		self.b = difference
+		self.lenght.list()
+
+	def list(self):
+		for index, number in enumerate(self.a):
+			if index< self.length-1:
+				self.a[index+1]-self.a[index]==1
+				list.append(number)
+		return list
+				
+	
+class BinarySearch(ListComprehension):
+  def search(self, item):
+		self.item=item
+		dictionary={}
+	
+		if self.a==0:
+			return False 
+		else:
+			dictionary[item]+=1
+		
+
+
+
